32c3204ec54bf8b66258148625dc718c9307f5fa
Minkyung <kyoonge98@gmail.com>
2024-12-10 01:48:37 +0900

    카메라 모드, 플레이 모드

---
+++ Assets/Scripts/GameModeManager.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+public class GameModeManager : MonoBehaviour
+{
+    public static GameModeManager Instance { get; private set; }
+
+    public enum GameMode
+    {
+        Play,
+        Camera
+    }
+
+    public GameMode CurrentMode { get; private set; }
+    public UnityEvent<GameMode> OnGameModeChanged = new UnityEvent<GameMode>();
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+
+        CurrentMode = GameMode.Play;
+    }
+
+    public void ToggleMode()
+    {
+        CurrentMode = CurrentMode == GameMode.Play ? GameMode.Camera : GameMode.Play;
+        OnGameModeChanged.Invoke(CurrentMode);
+    }
+
+    public bool IsPlayMode() => CurrentMode == GameMode.Play;
+    public bool IsCameraMode() => CurrentMode == GameMode.Camera;
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,105 @@
+using UnityEditor.Rendering;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using static UnityEditor.SceneView;
+
+public class PlayerController : MonoBehaviour
+{
+    [Header("Movement Settings")]
+    [SerializeField] private float moveSpeed = 5f;
+    [SerializeField] private float jumpForce = 5f;
+    [SerializeField] private float mouseSensitivity = 2f;
+
+    [Header("Camera Settings")]
+    [SerializeField] private Transform cameraTransform;
+    [SerializeField] private float maxLookAngle = 80f;
+    [SerializeField] private GameObject cameraUI;
+    [SerializeField] private GameObject cameraModel;
+
+    private PlayerInput playerInput;
+    private Rigidbody rb;
+    private Vector2 moveInput;
+    private Vector2 lookInput;
+    private float cameraPitch;
+
+    private bool isCameraMode;
+
+    private void Awake()
+    {
+        playerInput = GetComponent<PlayerInput>();
+        rb = GetComponent<Rigidbody>();
+
+        if (cameraTransform == null)
+            cameraTransform = Camera.main.transform;
+
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        moveInput = context.ReadValue<Vector2>();
+    }
+
+    public void OnLook(InputAction.CallbackContext context)
+    {
+        lookInput = context.ReadValue<Vector2>();
+    }
+
+    public void OnModeChange(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            GameModeManager.Instance.ToggleMode();
+        }
+    }
+
+    private void Start()
+    {
+        GameModeManager.Instance.OnGameModeChanged.AddListener(HandleModeChanged);
+    }
+
+    private void HandleModeChanged(GameModeManager.GameMode newMode)
+    {
+        isCameraMode = newMode == GameModeManager.GameMode.Camera;
+        cameraUI.SetActive(isCameraMode);
+        //cameraModel.SetActive(isCameraMode);
+    }
+
+    public void OnAttack(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            Attack();
+        }
+    }
+
+    private void Update()
+    {
+        HandleMovement();
+        HandleLook();
+    }
+
+    private void HandleMovement()
+    {
+        Vector3 move = transform.right * moveInput.x + transform.forward * moveInput.y;
+        transform.position += move * moveSpeed * Time.deltaTime;
+    }
+
+    private void HandleLook()
+    {
+        // Horizontal rotation (Player rotation)
+        transform.Rotate(Vector3.up * lookInput.x * mouseSensitivity);
+
+        // Vertical rotation (Camera pitch)
+        cameraPitch -= lookInput.y * mouseSensitivity;
+        cameraPitch = Mathf.Clamp(cameraPitch, -maxLookAngle, maxLookAngle);
+        cameraTransform.localRotation = Quaternion.Euler(cameraPitch, 0, 0);
+    }
+
+    private void Attack()
+    {
+        Debug.Log("Attack performed!");
+        // Add attack logic here
+    }
+}
